ac91b79c2642c9ccd7ad65dbfc8af4da7c76d2b1
manhattan404 <76757456+manhattan404@users.noreply.github.com>
2021-07-03 18:53:01 -0400

    Update

---
+++ 6_Events.js
@@ -0,0 +1,10 @@
+// EVENT HANDLERS
+
+// in the video, Traversy created a new button under the list
+// He's using emmet + tab for shortcuts
+
+// Emment syntax he used in the html file under <ul>
+    // button.btn.btn.-dark#button
+    // this is inside an HTML file
+    <button onclick='alert(1)' class="btn btn-dark btn-block" id="button">Click Here</button>
+    // Will add a dark button  and will display 1 when you click it 
